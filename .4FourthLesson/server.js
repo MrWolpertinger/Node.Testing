ac2d43fe9e45a0cc383d28737efb7a72e196d5ac
Derek Kropff <70429729+MrWolpertinger@users.noreply.github.com>
2021-06-08 12:56:48 +0200

    Completed Losson four - D Kropff

--- .4FourthLesson/server.js
+++ .4FourthLesson/server.js
@@ -5,16 +5,42 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     //set header content type
+
+
     //res.setHeader('Content-Type', 'text/html');
     
     //res.write('<head><link rel ="stylesheet" href ="#"></head>');
     //res.write('<p>Hello, mythos.</p>');
     //res.write('<p>Hello again, mythos.</p>');
     //res.end();
+
+
+    
     res.setHeader('Content-Type', 'text/html');
     
+    let path = './views/';//to switch what html file gets displayed
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me': //redirect
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
     //send HTML file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
